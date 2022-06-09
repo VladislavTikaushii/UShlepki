0fdbb44b124cfa309fa0a12655486a1851bea0c3
VladislavTikaushii <Vladik.god0_0@mail.ru>
2022-06-10 00:33:02 +0300

    ыыы

---
+++ LichniiCabinet.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title> ЛК У Шлёпки </title>
+  </head>
+  <body>
+    <div class="navbar">  Строка навигации
+    </div>
