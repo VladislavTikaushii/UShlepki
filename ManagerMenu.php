e97feed8b7681b78a64e17db36416bfbbbc57247
VladislavTikaushii <Vladik.god0_0@mail.ru>
2022-06-10 01:48:19 +0300

    ыыы6

---
+++ ManagerMenu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title> Редактор У Шлёпки </title>
+  </head>
+  <body>
+    <div class="infa"> Тут инфа для менеджера 
+      <div> таблица с персоналом
+        работники
+        должности и их зарплаты
+      </div>
+      <div> таблица курьра
+        время доставок
+        адреса
+      </div>
+    </div>
+  </body>
+</html>
