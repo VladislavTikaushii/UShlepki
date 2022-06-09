50d3f60bb53cde92d9cf7219fc78e439a0be9594
VladislavTikaushii <Vladik.god0_0@mail.ru>
2022-06-10 01:17:48 +0300

    sss3

---
+++ Karzina.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title> Корзинка У Шлёпки </title>
+  </head>
+  <body>
+    <div class="Karzinka"> Тут заказ и оплата заказа.
+      <div> Никита тут спамящиеся квадраты - то что заказал посититель </div>
+      <div> Тут область где адрес указан </div>
+      <div> Тут расчёт цены к оплате и доставки </div>
+    </div>
+  </body>
+</html>
