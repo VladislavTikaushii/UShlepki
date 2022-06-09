183ca2257b52f6d3aeea89ad2fa22669c8c73864
VladislavTikaushii <Vladik.god0_0@mail.ru>
2022-06-10 00:32:18 +0300

    Первы комит деду

---
+++ Sklad.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title> Склад У Шлёпки </title>
+  </head>
+  <body>
+    <div class="navbar">  Строка навигации
+    </div>
+    <div class="tablitsya"> <!-- Тут таблица с ресурсами -->
+      Тут таблица со столбцами(это таблица ресурсов на складе)
+      Тесто
+      фарш телячий
+      фарш куриный
+      фарш свиной
+      соль
+      перец
+    </div>
+  </body>
